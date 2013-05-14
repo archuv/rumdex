0df4b581dac7b192ce99c143b67cd47575525131
archuv <hqyeamin@gmail.com>
2013-05-14 20:42:03 +0600

    install php5

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
+#
+
+import os,sys,subprocess
 def main():
-	print "hello"
+    print "Install mysql......."
+    #subprocess.call(["zypper","install","mysql-community-server])
+    #subprocess.call(["zypper","install","postfix","postfix-mysql","mysql-community server"])
+    #subprocess.call(["systemctl","enable","mysql.service"])
+    #subprocess.call(["rcmysql","reload"])
+    #subprocess.call(["mysql","secure","installation"])
+    print "Installing apache2......"
+    #subprocess.call(["zypper","install","apache2"])
+    #subprocess.call(["systemctl","enable","apache2.service"])
+    #subprocess.call(["rcapache2","reload"])
+    print "Installing php5........"
+    #subprocess.call(["zypper","install","apache2-mod_php-5"])
+    #subproces.call(["rcapache2","reload"])
+def testmatch():
+    file_info_php=open('/srv/www/htdocs/info.php', 'w')  
+    
 if __name__ == "__main__":
-	main()
+     main()
+     testmatch()
